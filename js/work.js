9b1070fb1473eb009ec577e8ead0915b2845fe76
mattward0028 <mattward0028@gmail.com>
2016-12-13 13:41:51 -0800

    first commit

---
+++ js/work.js
@@ -0,0 +1,41 @@
+var works = [
+  {
+    title: "Ecommerce",
+    pic: "img/ecommerce.png",
+    url: "https://myfirstapp0028.herokuapp.com/"
+  },
+  {
+    title: "Portfolio",
+    pic: "img/portfolio.png",
+    url: "http://fisherman-sheep-78868.bitballoon.com/"
+  },
+  {
+    title: "Calc",
+    pic: "img/calc.png",
+    url: "http://treasurer-ride-16486.bitballoon.com/"
+  },
+  {
+    title: "Blog",
+    pic: "img/blog.png",
+    url: "https://wardblog.herokuapp.com"
+  }
+];
+
+var works2 = [
+  {
+    title: "Fifth Project",
+    pic: "img/html5&css3.jpg",
+  },
+  {
+    title: "Sixth Project",
+    pic: "img/JavaScript.jpg",
+  },
+  {
+    title: "Seventh Project",
+    pic: "img/GitHub.jpg",
+  },
+  {
+    title: "Eighth Project",
+    pic: "img/RubyOnRails.jpg"
+  }
+];
